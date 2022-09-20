d6efdbe95add47f89b816bd1d31ac18ad86baea8
Hafiffdl <hafif.fdl16@gmail.com>
2022-09-20 12:08:35 +0700

    Pertemuan 2

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Hafif Faadillah
+            Saya tinggal di daerah tangerang selatan
+            olah raga yang saya sukai adalah sepakbola";
+    }
+}
